87ef9e9e40b325b7b41a2176d358ba064125bab2
Felipe <felipescherer25@gmail.com>
2021-04-07 11:15:49 -0300

    Test zoom

---
+++ src/tests/TesteScrollPanel.java
@@ -0,0 +1,15 @@
+package tests;
+
+import javax.swing.JScrollPane;
+
+public class TesteScrollPanel {
+
+	public static void main(String[] args) {
+		
+		
+		
+		frame sc = new frame();
+		
+		
+	}
+}
---
+++ src/tests/frame.java
@@ -0,0 +1,42 @@
+package tests;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.ScrollPane;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+
+public class frame extends JFrame{
+	
+	
+	public frame() {
+		
+		ScrollPane sp = new ScrollPane();
+		setSize(new Dimension(500, 500));
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+		this.add(sp);
+		sp.addMouseWheelListener(new zoomBehavior());
+		
+		sp.add(new JButton("Salve"), BorderLayout.CENTER);
+	}
+	
+}
+
+
+class zoomBehavior implements MouseWheelListener{
+
+	@Override
+	public void mouseWheelMoved(MouseWheelEvent e) {
+		
+		
+	}
+	
+	
+}
